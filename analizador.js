c3ef7e047b9e172836b1d7e98f484883eb038a55
unknown <andresgrandapiedrahita@gmail.com>
2019-05-16 08:40:11 -0500

    agregando expresiones regulares

--- analizador.js
+++ analizador.js
@@ -1,14 +1,29 @@
-let expresions= require('./expresiones.json')
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let expresions = require("./expresiones.json");
 
-  function analizadorSintac(txt) {
-    console.log(expresions)
+function analizadorSintac(txt) {
+  let tieneExpresion = false;
+  expresions.expresiones.forEach(expr => {
+    let reg = new RegExp(expr.expresion, "g");
+    let match = reg.exec(txt);    
+    if (match[0] === match["input"]) {
+      console.log(expr.nombre);
+      tieneExpresion = true;
+    }
+  });
+  if (!tieneExpresion) {
+    console.log("Error: El dato no se reconoce");
   }
-  
-  readline.question('Ingrese el texto que desea analizar : ', txt => {
+}
+
+function getData() {  
+  console.log("------------------Menu inicial---------------------3");
+  readline.question("Ingrese el texto que desea analizar : ", txt => {
     analizadorSintac(txt);
-    readline.close()
-  })
+    readline.close();
+  });
+}
+getData();
